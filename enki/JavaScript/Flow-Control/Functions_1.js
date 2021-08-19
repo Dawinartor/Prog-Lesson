2054e1d31119253c69cfa99673fe1ba21168748f
Dawinartor <46395323+Dawinartor@users.noreply.github.com>
2021-08-19 22:58:11 +0200

    top-down interpreting

--- enki/JavaScript/Flow-Control/Functions_1.js
+++ enki/JavaScript/Flow-Control/Functions_1.js
@@ -34,3 +34,49 @@ changeObj(obj);
 console.log(obj.name);
 //-> Gilt jedoch nur bei nicht "primitiven" Datentypen wie Objekten oder Arrays
 
+//? Was ist der Unterschied zwischen Parameter und Argument
+//! 1. Ein Parameter witd als Eingabeplatzhalter bei Funktionen verwendet
+//! 2. Ein Argument ist der Wert, welcher zur weiterverarbeitung, der funktion uebergeben wird.
+
+//? Was ist ein pseudo-array?
+//! Ein Objekt mit einer laenge, welches jedoch nicht die Funktionen eines Arrays in JS mitbringt.
+
+function sortedArgs() {
+    console.log(Array.isArray(arguments)); //-> false, weil es ein pseudo-Array ist.
+
+    // Kopiere Inhalte aus Pseudo-Array in echtes Array
+    let argList = Array.from(arguments);
+    console.log(Array.isArray(argList)); //-> true.
+}
+//-> Ohne, dass Parameter erwartet werden kann auf diese mit "arguments" zugegriffen werden.
+sortedArgs(5, 2, 7);
+
+function myFunc() {
+    console.log(arguments[0]);
+    console.log(arguments[1]);
+    console.log(arguments[2]);
+}
+//-> Hier nochmal besser zu sehen
+myFunc(5,4,3);
+
+// Gebe ein Array mit uebergebenen Werten zurueck
+function returnArgs() {
+    return Array.from(arguments);
+}
+
+// Die Funktions deklaration wird stehts vor der verwendung geladen unabhängig von der Position im code
+console.log(useBeforeDeclaration());
+function useBeforeDeclaration() {
+    return "Say hello to me";
+}
+
+/**
+* Bei der Verwendung von Funktions Ausdruecken ist, dass jedoch nicht moeglich.
+* Da lauft es von oben nach unten
+*/ 
+//!console.log(zeiger);
+//!let zeiger = function() {return 'Ich Zeige dir den Weg';} //! Fehler
+
+//? Wie verwendet man "short circuiting"?
+
+
