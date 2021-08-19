dfdff1b31e5222666fdb1d5d4aa7cd60945ce697
Dawinartor <46395323+Dawinartor@users.noreply.github.com>
2021-08-19 22:14:02 +0200

    Pass by value & Pass by reference

--- enki/JavaScript/Flow-Control/Functions_1.js
+++ enki/JavaScript/Flow-Control/Functions_1.js
@@ -7,15 +7,30 @@
      // -> Kommt ein "undefined" zurueck
  }
 
- add(1, 2);
+ // add(1, 2);
 
  //? Was ist "Pass by value"
  //! Es werden nicht die originalen Daten verabeitet sondern eine Kopie dieser
+ //-> Gilt jedoch nur fuer "primitive" Datentypen wie Strings oder integers
 
  //? Wenn wir ein "Pass by value" Objekt manipulieren was kommt dann beim original zurueck?
- var a1 = 5;
- var b1 = 3;
+function changeValue(value) {
+    return value = 1000;
+}
 
- add(a1, b1);
- console.log(a1);
- console.log(b1);
+var a1 = 5;
+console.log(changeValue(a1));
+console.log(a1); //! Es wird nur eine Kopie des Wertes verwendet, damit bleibt der urspruengliche Wert immer erhalten.
+
+//? Was ist "Pass by reference"
+//! Es wird statt einer Kopie der originale Wert verwendet bzw. die Referenz des Wertes verwnedet.
+
+function changeObj(object) {
+    object.name = "Apple";
+}
+
+let obj = {name: "Enki"};
+changeObj(obj);
+console.log(obj.name);
+//-> Gilt jedoch nur bei nicht "primitiven" Datentypen wie Objekten oder Arrays
+
