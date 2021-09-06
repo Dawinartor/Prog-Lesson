6a2877a62362740783a2c0f6e66e74050f51f76b
David Melamed <david.melamed@web.de>
2021-09-06 20:07:58 +0300

    Learned new things

--- enki/JavaScript/Kapitel_2/Scope.js
+++ enki/JavaScript/Kapitel_2/Scope.js
@@ -13,7 +13,39 @@ go(); // undefined
 var a;
 a = 1;
 function go() {
-    var a;
+//    var a;    // Shadowing: a ist global verfuegbar, jedoch wird a erneut in neuem Scope definiert und damit bleibt unerreichbar.
     console.log(a);
     a = 2;
 }
+
+function show(x) {
+    console.log(x);
+}
+
+show(a);
+
+s = "enki"; // 2. Dann zugeviesen
+console.log(s); // 3. Dann anzeigen
+var s; // 1. Kommt hoch
+
+
+let b = 1;
+
+function foo() {
+     if(true) {
+         var b = 3; // Ich erhalte 3, weil "var" Werte im ganzen function "Scope" erreichbar sind.
+     }              // Unabhaengig ob die Funktion weitere Scopes hat oder nicht.
+     console.log(b);
+}
+
+foo();
+
+// Scope & Strict Mode
+function bar(foo) {
+    foo = "hello";
+    baz = "world";
+    console.log(foo); // Ohne strict Modus is foo: Lokal & baz: Global
+    console.log(baz);
+}
+
+bar();
