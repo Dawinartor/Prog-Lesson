5cdccefda118b57495fd44199ebf668b5b9c5222
David Melamed <david.melamed@web.de>
2021-09-10 00:09:41 +0300

    Add lots of DocStrings

--- TaskJS/Validation.js
+++ TaskJS/Validation.js
@@ -1,6 +1,8 @@
-function Validation(input="", constraints=null, level=0){
+'use strict;'
+
+function Validation(input="", constraints=null, level=0) {
 /**
- * Validates input by given array
+ * Validates input by given constraints depending on level
  * @param {string} input String which have to be validate
  * @param {array.<string>} [constraints] Array of strings with constraints to check for
  * @param {number} [level] Level of checking 
@@ -14,6 +16,29 @@ const level = {
     ending: 3, // cannot end with an underscore character.
     all: 4 //
 }
+
+// check level and act 
+switch(level) {
+    case 0:
+        // Check how many & which constrains
+        // check input for constraints
+    case 1:
+        // Check how many & which constrains
+        // check input for constraints
+    case 2:
+        // Check how many & which constrains
+        // check input for constraints
+    case 3:
+        // Check how many & which constrains
+        // check input for constraints
+    case 4:
+        // Check how many & which constrains
+        // check input for constraints
+    default:
+        console.log("Something went wrong by parameter 'level'");
+        return "Something went wrong by parameter 'level'";
+}
+
 
 
 }
