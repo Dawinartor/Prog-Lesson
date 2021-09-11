1395f2665948679563802e25159697c5a56a00d2
David Melamed <david.melamed@web.de>
2021-09-11 13:45:18 +0300

    Where is the problem?

--- TaskJS/test/testCaseCharacters.js
+++ TaskJS/test/testCaseCharacters.js
@@ -2,5 +2,15 @@ import { validate } from '../validate.js';
 import chai from 'chai';
 
 var assert = chai.assert;
-assert(validate("H3ll0#", [5, 13], 1) == [true, "H3ll0#"], "Test case failed LALALLALALAL");
-// TODO: Check why it failed?!
+var expectedValues = {
+    validate: true,
+    input: "H3ll0#"
+};
+
+console.log(expectedValue)
+
+let expectedValue = {expectedValues[validate] , expectedValues.input};
+//! WHAT is wrong here?
+assert.equal(validate("H3ll0#", [5, 13], 1), expectedValue, "Test case successfull TROLLOLOLOLO");
+assert.equal(validate("H3ll0#", [5, 13], 1), expectedValue, "Test case successfull TROLLOLOLOLO");
+
