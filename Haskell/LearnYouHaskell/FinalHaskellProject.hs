52c58ad573481c65ba43701c2566d3fbc5e783e7
David Melamed <david.melamed@web.de>
2021-10-04 16:07:16 +0300

    update

--- Haskell/LearnYouHaskell/FinalHaskellProject.hs
+++ Haskell/LearnYouHaskell/FinalHaskellProject.hs
@@ -1,3 +1,15 @@
--- 1- Find a suitable and interesting topics from one of sustainable projects
--- 2- Choose a best public dataset
--- 3- Use Haskel to predict the target
+-- 1) Find a suitable and interesting topics from one of sustainable projects
+-- 2) Choose a best public dataset
+-- 3) Use Haskel to predict the target
+-- 4) Select your teamwork
+-- 5) Give me the name of your groups and include all members
+
+-- G1- A,B,C
+
+-- 6) Documentation
+  -- Title of project
+  -- Team member
+  -- dataset desciption
+  -- Problem statement
+  -- objective
+  -- Methods
